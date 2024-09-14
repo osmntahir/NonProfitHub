165b5bbf817835e964c42e36f1f0f9020b0553d2
Osman Tahir Özdemir <ozdemirosmantahir@gmail.com>
2024-09-14 15:06:03 +0300

    Initial commit

---
+++ context-manager/src/main/java/com/cmvbilisim/contextmanager/ContextManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cmvbilisim.contextmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContextManagerApplication.class, args);
+    }
+
+}
---
+++ context-manager/src/test/java/com/cmvbilisim/contextmanager/ContextManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmvbilisim.contextmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
