7d815dd1fad3cc6f2a7ec54d188f80e87b40e02a
Osman Tahir Özdemir <ozdemirosmantahir@gmail.com>
2024-09-20 16:45:55 +0300

    Added some design improvements to the admin panel.

--- admin-panel/src/App.vue
+++ admin-panel/src/App.vue
@@ -2,9 +2,12 @@
   <div class="app-container">
     <!-- Sidebar -->
     <aside class="sidebar">
+      <div class="sidebar-header">
+        <h1>Admin Panel</h1>
+      </div>
       <ul>
-        <li @click="goToPage('news')">Haberler</li>
-        <li @click="goToPage('announcements')">Duyurular</li>
+        <li @click="goToPage('news')" :class="{ active: isActivePage('news') }">Haberler</li>
+        <li @click="goToPage('announcements')" :class="{ active: isActivePage('announcements') }">Duyurular</li>
       </ul>
     </aside>
 
@@ -20,41 +23,104 @@ export default {
   methods: {
     goToPage(page) {
       this.$router.push(`/${page}`);
+    },
+    isActivePage(page) {
+      return this.$route.path.includes(page);
     }
   }
 };
 </script>
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body {
+  height: 100%;
+  overflow-x: hidden;
+}
+
 .app-container {
   display: flex;
+  min-height: 100vh;
+  font-family: 'Arial', sans-serif;
 }
 
 .sidebar {
-  width: 200px;
-  background-color: #333;
-  color: white;
+  width: 250px;
+  background-color: #142333;
+  color: #fff;
   padding: 20px;
-  height: 100vh;
   position: fixed;
   top: 0;
   left: 0;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.sidebar-header h1 {
+  font-size: 1.8em;
+  color: #3fc380;
+  font-weight: bold;
+  text-align: center;
+}
+
+.sidebar-header {
+  margin-bottom: 60px; /* Increased margin to push the list down */
+  text-align: center;
 }
 
 .sidebar ul {
   list-style: none;
   padding: 0;
+  margin: 0;
+  flex-grow: 1;
 }
 
+
 .sidebar li {
-  margin: 20px 0;
+  font-size: 1.2em;
+  margin: 10px 0; /* Added margin to separate the list items further */
   cursor: pointer;
-  color: white;
+  padding: 10px 20px;
+  border-radius: 6px;
+  transition: background-color 0.3s ease;
+}
+
+.sidebar li:hover, .sidebar li.active {
+  background-color: #3fc380;
 }
 
 .main-content {
-  margin-left: 220px;
+  margin-left: 250px;
   padding: 20px;
-  width: calc(100% - 220px);
+  background-color: #f0f2f5;
+  width: calc(100% - 250px);
+  height: 100vh;
+  overflow-y: auto;
+}
+
+.main-content h1 {
+  font-size: 2.2em;
+  margin-bottom: 20px;
+  color: #333;
+}
+
+.main-content button {
+  background-color: #28a745;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.main-content button:hover {
+  background-color: #218838;
 }
 </style>
